450af39f573ee916bb7519a21ed94fa694b13cca
Brett Schumann <brett.schumann@t101.com>
2017-06-26 10:17:39 +0100

    Init Commit

---
+++ ReconMessages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReconMessages
+//
+//  Created by Brett Schumann on 26/06/2017.
+//  Copyright © 2017 Brett Schumann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
